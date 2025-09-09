368e3e6687d7340217a281a44f3cbc037a512bf3
PERFEXION <chubuike17@gmail.com>
2025-09-09 18:22:12 +0100

    built a product page, the associated css and javascript pages
    updated navigation links

--- script.js
+++ script.js
@@ -53,4 +53,22 @@ menuIcon.addEventListener("click", () => {
 });
 
 
-// -----shoplistdetails-----
+// -----shop list details-----
+
+// -----img slider----
+
+var mainImage = document.getElementById("main_image")
+var smallImage = document.getElementsByClassName("small_image")
+
+smallImage[0].onclick = function () {
+  mainImage.src = smallImage[0].src
+}
+smallImage[1].onclick = function () {
+  mainImage.src = smallImage[1].src
+}
+smallImage[2].onclick = function () {
+  mainImage.src = smallImage[2].src
+}
+smallImage[3].onclick = function () {
+  mainImage.src = smallImage[3].src
+}
